24a4647bdfcf9844aa8aafa1384e058e8e161284
Hines Kao <hineskao@gmail.com>
2016-07-18 17:20:12 -0700

    Still working on facebookoauth

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'facebookAuth' : {
+      'clientID'      : '1766132520340607',
+      'clientSecret'  : '7707b54fe740f60ff307920272995ec6',
+      'callbackURL'   : 'https://shielded-waters-54112.herokuapp.com/'
+  },
+};
--- index.js
+++ index.js
@@ -6,10 +6,35 @@ var
   passport = require('passport'),
   flash = require('connect-flash'),
   morgan = require('morgan'),
-  cookieParser = require('cookie-parser')
-  dotevn = require('dotenv').load({silent: true}),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  configDB = require('./config/database.js'),
+  dotevn = require('dotenv').load({silent: true})
 
 mongoose.connect(process.env.DB_URL, function(err){
   if(err) return console.log(err);
   console.log("Connected to MongoDB (DatePage)");
 })
+
+// require('./config/passport')(passport);
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport);
+// load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(PORT);
+console.log('The magic happens on port ' + PORT);
